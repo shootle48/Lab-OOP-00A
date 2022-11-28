ee2ede54aa079c2f9eaa840742e96eea5ae61ca4
B-Man <lolicon2544@gmail.com>
2022-11-28 15:36:57 +0700

    Add quote to pragram

--- Lab-OOP-00A/Lab-OOP-00A/Program.cs
+++ Lab-OOP-00A/Lab-OOP-00A/Program.cs
@@ -1,6 +1,11 @@
 ﻿//Fname&Lname
 Console.Write("My name is Thepparit Intharapraphan\n");
+
 //St_Id
 Console.Write("My ID is 653450091-4\n");
+
 //kkumail
 Console.Write("My kkumail is Thepparit.i@kkumail.com\n");
+
+//quote
+Console.Write("You make me want to be a better man :3");
